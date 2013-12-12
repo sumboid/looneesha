d3a0bec9859fb0319c350252851296b6998c5965
Ilya Sumbatyants <ilya.sumb@gmail.com>
2013-12-12 10:54:07 +0700

    sync

--- src/internal/DF.scala
+++ src/internal/DF.scala
@@ -1,14 +1,13 @@
 package looneesha
 
-case class Counter(name: String, value: Int = 0, defined: Boolean = false) extends Fragment {
-}
-
-case class DF(name: String, index: Integer, value: Double = 0, defined: Boolean = false) extends Fragment {
-  override def equals (other: Any) = {
-    other.isInstanceOf[DF] &&
-    this.name == other.asInstanceOf[DF].name &&
-    this.index == other.asInstanceOf[DF].index
+case class Counter(name: String, value: Int = 0, defined: Boolean = false) extends Fragment
+case class DF extends Fragment
+case class AtomDF(name: String, index: Integer = 0, value: Double = 0, defined: Boolean = false) extends DF {
+  override def equals (other: Any) = other match {
+    case x: AtomDF => this.name == x.name && this.index == x.index
+    case _ => false
   }
 
-  def set (v: Double) = DF(name, index, v, true)
+  def set (v: Double) = AtomDF(name, index, v, true)
 }
+case class MetaDF(name: String) extends DF
