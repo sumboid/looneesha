d0c93aa1044e7d50c1d2b68a1c8230d601ad92f1
Ilya Sumbatyants <ilya.sumb@gmail.com>
2013-12-12 14:24:50 +0700

    refactoring

--- src/runtime/Runtime.scala
+++ src/runtime/Runtime.scala
@@ -4,20 +4,30 @@ import scala.actors.Actor
 
 class Runtime(graph: Graph, input: List[AtomDF], output: List[AtomDF]) extends Actor {
   var actors: List[Actor] = Nil
+  var out = output.toArray
 
   //def visualize = GV.create(graph, Graph(graph.paths(input, output).flatMap(_.cfs)), input, output).draw
-  def writeData = { 
+  def writeData = {
     println("Graph: " + graph)
     println("Input: " + input)
     println("Output: " + output)
   }
+
   def writeSolution = println("Paths: " + graph.paths(input, output))
 
-  def act = {
-    loop {
-      react {
-        case df: AtomDF => println("Look! " + df.name + " = " + df.value)
-        case _ => println("ugh?") 
+  def defineDF(df: AtomDF) = {
+    val index = out indexOf df
+    out update (index, df)
+  }
+
+  def end = out forall (_.defined)
+
+  def act = loop {
+    react {
+      case df: AtomDF => {
+        println("Look! " + df.name + " = " + df.value)
+        defineDF(df)
+        if(end) exit
       }
     }
   }
@@ -52,25 +62,28 @@ object Runtime {
 }
 
 case class CFRuntime(cf: CF, link: List[(AtomDF, Actor)]) extends Actor {
-  println("Created actor for " + cf.name)
-  def act = {
-    var input = cf.in.toArray
-    loop {
-      println(cf.name + ": waiting for next AtomDF")
-      react {
-        case df: AtomDF => {
-          println(cf.name + ": get new df: " + df.name)
-          val index = input.indexOf(df)
-          input update (index, df)
-          if (input forall (_.defined)) {
-            println(cf.name + ": everything is done!")
-            val realcf = cf.set(input.toList)
-            val result = realcf.run
-            result foreach (df => link filter (l => l._1 == df) foreach (l => l._2 ! df))
-            exit()
-          }
-        }
-      }
+  private[this] var input = cf.in.toArray
+
+  def defineDF(df: AtomDF) = {
+    val index = input indexOf df
+    input update (index, df)
+  }
+
+  def tryRun = if (input forall (_.defined)) {
+      val realcf = cf.set(input.toList)
+      val result = realcf.run
+      result foreach (df => link filter (l => l._1 == df) foreach (l => l._2 ! df))
+      exit
+  }
+
+  def defineAndTryRun(df: AtomDF) = {
+    defineDF(df)
+    tryRun
+  }
+
+  def act = loop {
+    react {
+      case df: AtomDF => defineAndTryRun(df)
     }
   }
 }
